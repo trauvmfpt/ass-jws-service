9a42b46c9daeab035b3598568c0543d5806863ac
JUstSaLtPiLe <vu.tienphuc97@gmail.com>
2019-10-28 01:10:07 +0700

    sua lai dto post va place de loi child ra, sua lai config hibernate de no khon hon, sua service gi ko nho

--- src/main/java/dto/PlaceDTO.java
+++ src/main/java/dto/PlaceDTO.java
@@ -1,16 +1,38 @@
 package dto;
 
 import entity.Place;
+import entity.Post;
+import entity.Role;
 import util.ObjectUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaceDTO {
     private int id;
     private String name;
     private String address;
     private int status;
+    private List<PostDTO> posts;
 
     public PlaceDTO(Place place) {
         ObjectUtil.cloneObject(this,place);
+        if(place.getPostSet() != null){
+            for (Post p : place.getPostSet()) {
+                if (this.posts == null){
+                    this.posts = new ArrayList<>();
+                }
+                this.posts.add(new PostDTO(p));
+            }
+        }
+    }
+
+    public List<PostDTO> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<PostDTO> posts) {
+        this.posts = posts;
     }
 
     public int getId() {
